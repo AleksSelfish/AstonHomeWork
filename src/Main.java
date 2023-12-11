cdc0e14c8f449f21f14d9ddd0a2dc451f0d6831a
Alex Perov <bzzname@icloud.com>
2023-12-12 00:05:58 +0300

    /

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-class Main{
+class Main {
     public static void main(String[] args) {
+        MyArray<Object> a = new MyArray<>(2);
+        MyArray<Object> b = new MyArray<>(2);
+        a.add(2);
+        a.add(3);
+        a.add(5);
+        a.remove(1);
+        b.add(32);
+        b.add(a);
+        a.addAll(b);
+
+
+        System.out.println(a.size());
+        System.out.println(a.get(3));
+
+
+        }
 
 
     }
-}
+
+
--- src/MyArray.java
+++ src/MyArray.java
@@ -1,17 +1,47 @@
-public class MyArray<Obj> {
-    private final Object[] elements;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyArray<E> {
+    private Object[] elements;
     private int size;
 
     MyArray(int startValue) {
         this.elements = new Object[startValue];
 
     }
+    public boolean addAll(MyArray<E> list) {
+        try {
+            Object[] temp = elements;
+            elements = (Object[]) new Object[temp.length + 1];
+            for (int i = 0; i <= list.size; i++) {
+                add(list.elements[i]);
+            }
+            System.arraycopy(temp, 0, elements, 0, temp.length);
+            size++;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
 
-    public void add(Object elem) {
-        elements[size] = elem;
-        size++;
+        }
+
+        return false;
 
     }
+
+    public boolean add(Object elem) {
+        try {
+            Object[] temp = elements;
+            elements = (Object[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, elements, 0, temp.length);
+            elements[elements.length - 1] = elem;
+            size++;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+
+        return false;
+
+    }
+
 
     public Object get(int num) {
         return elements[num];
@@ -27,11 +57,22 @@ public class MyArray<Obj> {
         return size;
     }
 
-    public Object remove(int num) {
-        size--;
-        return elements[num] = null;
+    public Object remove(int index) {
+        try {
+
+
+            Object[] temp = elements;
+            elements = (Object[]) new Object[temp.length - 1];
+            System.arraycopy(temp, 0, elements, 0, index);
+            int amountAtefRemove = temp.length - index - 1;
+            System.arraycopy(temp, index + 1, elements, index, amountAtefRemove);
+            size--;
+        } catch (ClassCastException ex) {
+            ex.printStackTrace();
+        }
+
+        return false;
 
     }
-
 
 }
